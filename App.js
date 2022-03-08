e6b59387135ea9cb0c96f99f4075bc7a245db990
caitevanish <cait.evan@gmail.com>
2022-03-08 09:10:13 -0800

    feat: new message when running app

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ const app = express();
 //Simple Hello World Node App
 const APP_PORT = process.env.APP_PORT || 3000;
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello Caitlin and the rest of the world out there!");
 });
 app.listen(APP_PORT, () => {
   console.log(`Running app at port:${APP_PORT}`);
