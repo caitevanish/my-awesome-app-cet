f59c698a99f45c4722d73691ca84919bb10fd6df
caitevanish <cait.evan@gmail.com>
2022-03-08 08:18:01 -0800

    Created App.js file with run instructions

---
+++ App.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+//Simple Hello World Node App
+const APP_PORT = process.env.APP_PORT || 3000;
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+app.listen(APP_PORT, () => {
+  console.log(`Running app at port:${APP_PORT}`);
+});
